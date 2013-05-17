00bbd1fdc999376d6ddbb18a9a71655eef825e84
bedhed <clement@seizam.com>
2013-05-17 18:44:49 +0200

    UserIdentity rewritten

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -1,33 +1,68 @@
 <?php
 
-/**
- * UserIdentity represents the data needed to identity a user.
- * It contains the authentication method that checks if the provided
- * data can identity the user.
- */
 class UserIdentity extends CUserIdentity
 {
+    
+    /**
+	 * @var string email
+	 */
+	public $email;
+    
+    /**
+	 * @var int _id
+	 */
+    private $_id;
+    
+    /**
+	 * Constructor.
+	 * @param string $username username
+	 * @param string $password password
+	 */
+	public function __construct($email,$password)
+	{
+		$this->email=$email;
+		$this->password=$password;
+	}
+    
 	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
+	 * Authenticates a user based on {@link email} and {@link password}.
 	 * @return boolean whether authentication succeeds.
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
+        $user = User::model()->findByAttributes(array('email'=>$this->email));
+        if ($user === null) {
+            $this->errorCode = self::ERROR_UNKNOWN_IDENTITY;
+        } elseif (!CPasswordHelper::verifyPassword($this->password, $user->password)) {
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        } else {
+            $this->_id = $user->id;
+            
+            // We do not use state yet.
+            // $this->setState('title', $record->title);
+            
+            $this->errorCode = self::ERROR_NONE;
+        }
+        return $this->errorCode;
+	}
+    
+    /**
+	 * Returns the unique identifier for the identity.
+	 * This method is required by {@link IUserIdentity}.
+	 * @return int the unique identifier for the identity.
+	 */
+	public function getId()
+	{
+		return $this->email;
+	}
+
+	/**
+	 * Returns the email for the identity.
+	 * This method is required by {@link IUserIdentity}.
+	 * @return string the email for the identity.
+	 */
+	public function getEmail()
+	{
+		return $this->email;
 	}
 }
