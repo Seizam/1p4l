eb8d081cc96d16517d829eb0f9620e0632827200
Clément <clement@seizam.com>
2013-07-19 17:46:00 +0200

    Added urlReferrer to contact email

--- protected/views/email/contact.php
+++ protected/views/email/contact.php
@@ -11,6 +11,7 @@
 			Contact infos: <?php echo "{$form->name} &lt;{$form->email}&gt;"; ?><br />
 			Date: <?php echo gmdate("Y-m-d H:i:s") . ' GMT'; ?><br />
 			Ip: <?php echo Yii::app()->request->userHostAddress; ?><br />
+			Referrer: <?php echo Yii::app()->request->urlReferrer ?><br />
 			Agent: <?php echo CHtml::encode(Yii::app()->request->userAgent); ?><br />
 		</p>
 	</body>
